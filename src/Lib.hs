86acfe3e6b313c1703fb52caa20c46c25ac9ba76
Kiriakos <kiriakos.krastillis@pwc.com>
2018-03-26 23:47:47 +0200

    Add enpoints and handler names for Cats

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,9 +12,30 @@ import Data.Aeson.TH
 import Network.Wai
 import Servant
 import Fixtures (users)
-import Types (User)
+import Types (Cat)
 
-type API = "users" :> Get '[JSON] [User]
+type API =   "cats" :> Get '[JSON] [FullCat]
+        :<|> "cats" :> ReqBody '[JSON] Cat :> Post '[JSON] FullCat
+        :<|> "cats" :> Capture "id" Int :> ReqBody '[JSON] FullCat :> Put '[JSON] FullCat
+        :<|> "cats" :> Capture "id" Int :> DeleteNoContent '[JSON] NoContent
+        :<|> "cats" :> Capture "id" Int :> Get '[JSON] RelationalCat
+        
+server :: Server API
+server = getCats
+     :<|> createCat
+     :<|> updateCat
+     :<|> deleteCat
+     :<|> getRelCat 
+
+    where
+        getCats :: Handler [FullCat]
+        createCat :: Cat -> Handler FullCat
+        updateCat :: Int -> FullCat -> Handler FullCat
+        deleteCat :: Int -> Handler NoContent
+        getRelCat :: Int -> Handler RelationalCat
+
+        
+     
 
 app :: Application
 app = serve api server
@@ -22,6 +43,4 @@ app = serve api server
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
 
